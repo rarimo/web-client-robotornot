4eb5b2bf8864801ea03fa9fc93fb2dce283b65ed
lukachi <lukachisama@gmail.com>
2023-10-03 19:00:12 +0300

    mock loading and success prove components

--- src/pages/MainPage/components/10_ProofSubmitted/ProofSubmitted.tsx
+++ src/pages/MainPage/components/10_ProofSubmitted/ProofSubmitted.tsx
@@ -2,19 +2,12 @@ import './styles.scss'
 
 import { type FC, HTMLAttributes } from 'react'
 
-import { AppButton } from '@/common'
-import { ICON_NAMES } from '@/enums'
-
 type Props = HTMLAttributes<HTMLDivElement>
 
 const ProofSubmitted: FC<Props> = ({ className, ...rest }) => {
   return (
     <div className={['proof-submitted', className].join(' ')} {...rest}>
-      <AppButton
-        iconLeft={ICON_NAMES.metamask}
-        text={`Submit Proof`}
-        modification='border-circle'
-      />
+      {`SUCCESS`}
     </div>
   )
 }
--- src/pages/MainPage/components/9_ProofSubmittingLoader/ProofSubmittingLoader.tsx
+++ src/pages/MainPage/components/9_ProofSubmittingLoader/ProofSubmittingLoader.tsx
@@ -2,19 +2,12 @@ import './styles.scss'
 
 import { type FC, HTMLAttributes } from 'react'
 
-import { AppButton } from '@/common'
-import { ICON_NAMES } from '@/enums'
-
 type Props = HTMLAttributes<HTMLDivElement>
 
 const ProofSubmittingLoader: FC<Props> = ({ className, ...rest }) => {
   return (
     <div className={['proof-submitting-loader', className].join(' ')} {...rest}>
-      <AppButton
-        iconLeft={ICON_NAMES.metamask}
-        text={`Submit Proof`}
-        modification='border-circle'
-      />
+      {`ProofSubmittingLoader`}
     </div>
   )
 }
