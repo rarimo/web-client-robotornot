1331e4ca23dfe77f167f6f39129f997ae278ed9e
lukachi <lukachisama@gmail.com>
2023-11-29 14:12:09 +0200

    update rarime connector

--- src/contexts/MetamaskZkpSnapContext/MetamaskZkpSnapContext.tsx
+++ src/contexts/MetamaskZkpSnapContext/MetamaskZkpSnapContext.tsx
@@ -130,7 +130,7 @@ const MetamaskZkpSnapContextProvider: FC<HTMLAttributes<HTMLDivElement>> = ({
   }, [])
 
   const checkSnapExists = useCallback(async () => {
-    const _isSnapInstalled = await detectSnapInstalled(undefined, '1.0.1-rc.0')
+    const _isSnapInstalled = await detectSnapInstalled()
 
     setIsSnapInstalled(_isSnapInstalled)
 
@@ -138,7 +138,7 @@ const MetamaskZkpSnapContextProvider: FC<HTMLAttributes<HTMLDivElement>> = ({
   }, [])
 
   const connectOrInstallSnap = useCallback(async () => {
-    const snap = await enableSnap(undefined, '1.0.1-rc.0')
+    const snap = await enableSnap()
     const connector = await snap.getConnector()
     setConnector(connector)
   }, [])
